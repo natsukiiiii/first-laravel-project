ae109981ba4388cb3e05430b29b6441f8f5836fb
natsukiiiii <w.natsuki.3@gmail.com>
2021-11-17 21:39:37 +0900

    make create

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,7 @@
                 投稿一覧
             </div>
             <div class="card-body">
-                <h5 class="card-title">初めての投稿</h5>
+                <h5 class="card-title">1回目の投稿</h5>
                 <p class="card-text">油そばは春日亭も好きですが、kirinjiが一番好きです</p>
                 <a href="#" class="btn btn-primary">詳細</a>
             </div>
@@ -17,6 +17,9 @@
                 2日前
             </div>
         </div>
+        </div>
+        <div class="col-md-2">
+            <a href="{{ route('posts.create') }}" class="btn btn-primary">新規投稿</a>
         </div>
     </div>
 </div>
